d0970b0a2839597054a015e66ba9a0f18357883a
J_Hao <1923185303@qq.com>
2025-03-08 02:36:31 +0800

    Site updated: 2025-03-08 02:36:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/04/Github/测试/","2025/03/03/hello-world/","2025/03/04/深入前端/WebRTC实现本地视频通话全流程/","2025/03/04/深入前端/源码探究 runtime-core模块/","2025/03/04/深入前端/从零实现axios/"];function toRandomPost(){
+var posts=["2025/03/04/深入前端/WebRTC实现本地视频通话全流程/","2025/03/04/深入前端/从零实现axios/","2025/03/04/深入前端/源码探究 runtime-core模块/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
